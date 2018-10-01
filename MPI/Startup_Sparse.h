f4fc3822936864aa467974da9aa91b9594f31a66
Oscar Huang <oscarhuang1212@gmail.com>
2018-09-30 22:45:23 -0400

    modify file import method to using argv[]

--- MPI/Startup_Sparse.h
+++ MPI/Startup_Sparse.h
@@ -1,6 +1,3 @@
-#define csv_file_name "CSV/Sparse_100_100_unbalanced.csv"
-#define matrix_bin_file_name "Bin/Sparse_100_100_unbalanced.bin"
-#define vector_bin_file_name "Bin/V1_100.bin"
 #define nrow 100
 #define ncol 100
 #define npoints 999
